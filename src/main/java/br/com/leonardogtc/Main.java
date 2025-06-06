ed3ecbf69fe003ca8dab4c71c58453ee48b4abd3
Leonardo Gonçalves Tavares da Conceição <leonardogtc@gmail.com>
2025-06-06 00:13:53 -0300

    Entidade monitorada

--- src/main/java/br/com/leonardogtc/Main.java
+++ src/main/java/br/com/leonardogtc/Main.java
@@ -14,7 +14,9 @@ public class Main {
         EntityManager em = emf.createEntityManager();
 
         Pessoa p = em.find(Pessoa.class, 2);
-        System.out.println("Pessoa encontrada: " + p.getNome());
+        em.getTransaction().begin();
+        em.remove(p);
+        em.getTransaction().commit();
 
         System.out.println("Procedimento executado com sucesso!");
         em.close();
