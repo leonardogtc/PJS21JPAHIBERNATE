782d3ab02d15ea4fc2a24031cfb2c5e0c241ff76
Leonardo Gonçalves Tavares da Conceição <leonardogtc@gmail.com>
2025-06-06 00:07:32 -0300

    Localizar uma informação na tabela

--- src/main/java/br/com/leonardogtc/Main.java
+++ src/main/java/br/com/leonardogtc/Main.java
@@ -8,20 +8,15 @@ import br.com.leonardogtc.dominio.Pessoa;
 
 public class Main {
     public static void main(String[] args) {
-        // Exemplo de uso da classe Pessoa
-        Pessoa p1 = new Pessoa(null, "João Silva", "joao@gmail.com");
-        Pessoa p2 = new Pessoa(null, "Maria Oliveira", "maria@gmail.com");
-        Pessoa p3 = new Pessoa(null, "Carlos Souza", "carlos@gmail.com");
+        
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("aulajpamaven");
         EntityManager em = emf.createEntityManager();
 
-        em.getTransaction().begin();
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
-        em.getTransaction().commit();
-        System.out.println("Pessoas salvas com sucesso!");
+        Pessoa p = em.find(Pessoa.class, 2);
+        System.out.println("Pessoa encontrada: " + p.getNome());
+
+        System.out.println("Procedimento executado com sucesso!");
         em.close();
         emf.close();
 
